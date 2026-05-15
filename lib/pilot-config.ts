5ee560a54fca293e1793df649acd38ef6a8dd7c2
Prabhav Pandey <prabhav@grapevine.in>
2026-05-15 17:56:32 +0530

    data: views from sheets (aarchi 3.97L, dot 4.79L)

--- lib/pilot-config.ts
+++ lib/pilot-config.ts
@@ -7,9 +7,9 @@ export type PilotMeta = {
 
 export const PILOT_META: Record<string, PilotMeta> = {
   'eastern-monk': { linkrunnerUrl: 'https://link.tal.af/?c=osBZBZ', budget: 177000 },
-  'aarchi':       { linkrunnerUrl: 'https://link.tal.af/?c=STKnPc', budget: 60000,  videoCount: 72  },
+  'aarchi':       { linkrunnerUrl: 'https://link.tal.af/?c=STKnPc', budget: 60000,  videoCount: 72,  views: 397579 },
   'yoursbossy':   { linkrunnerUrl: 'https://link.tal.af/?c=lbOtUm' },
-  'dot':          { linkrunnerUrl: 'https://link.tal.af/?c=LAtBdF', budget: 283200, videoCount: 75  },
+  'dot':          { linkrunnerUrl: 'https://link.tal.af/?c=LAtBdF', budget: 283200, videoCount: 75,  views: 479119 },
   'the-other':    { linkrunnerUrl: 'https://link.tal.af/?c=glrEWM', budget: 288500 },
   'third-draft':  { linkrunnerUrl: 'https://link.tal.af/?c=wPPtfW', budget: 236000, videoCount: 120 },
 }
